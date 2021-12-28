cc2ca1c3d265c57cd72306846d3ebb9b81701882
VitaL <vitaldukm@gmail.com>
2021-12-28 16:47:52 +0000

    Spotify Top100

---
+++ main.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+import config
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import pprint
+import sys
+
+SPOTIFY_ID = config.spotify_id
+SPOTIFY_SECRET = config.spotify_secret
+
+user_date = input("Which year do you want to travel to? Please type the date in this format YYYY-MM-DD: ")
+
+
+response = requests.get(f"https://www.billboard.com/charts/hot-100/{user_date}/")
+
+song_web_page = response.text
+
+soup = BeautifulSoup(song_web_page, "html.parser")
+
+list_of_artists_html = soup.find_all(name="h3", id="title-of-a-story", class_=["u-line-height-125"])
+# print(list_of_artists)
+list_of_songs_html = soup.find_all(name="span", class_="a-no-trucate")
+
+list_of_artists = [i.getText().strip("\n") for i in list_of_artists_html]
+list_of_songs = [i.getText().strip("\n") for i in list_of_songs_html]
+
+song_dict = dict(zip(list_of_artists, list_of_songs))
+
+spotify = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=SPOTIFY_ID,
+        client_secret= SPOTIFY_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = spotify.current_user()["id"]
+
+print(song_dict)
+
+tracks = []
+track_list = []
+song_uri = []
+
+for k, v in song_dict.items():
+    result = spotify.search(q=k + " " + v, type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uri.append(uri)
+    except IndexError:
+        print(f"The song {k}:{v} does not exist")
+
+
+print(len(song_uri))
+
+
+print(len(track_list))
+
+playlist = spotify.user_playlist_create(user=user_id, name=f"{user_date} Top 100 Songs!", public=False)
+added_tracks = spotify.playlist_add_items(playlist_id=playlist["id"], items=song_uri)
